3755400b61b9abc154b562ef3286e751054dc56d
Amanda Garcia <algarcian@outlook.com>
2019-05-16 11:33:20 -0600

    Added CSS Framework, Semantic UI, Faker gem data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# locations = ["SLC", "Austin", "San Antonio"]
+10.times do
+  Person.create(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    avatar: Faker::TvShows::Seinfeld.character, 
+    phone: Faker::PhoneNumber.cell_phone, 
+    email: Faker::Internet.email
+    # location: locations.sample; 
+    )
+end 
+
+# puts "10 People Created"
+# sleep(3)
+# print `clear`
+
+
